191febad9bf425a14c69c497c42862cdd9a11c7b
AlexJunior <alexmomijunior@gmail.com>
2023-03-30 15:54:46 -0300

    Innitial commit

---
+++ contracts/CertificateIssuer.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0; // versão do compilador do Solidity que será utilizado
+
+import "hardhat/console.sol";
+
+
+contract CertificateIssuer {
+    
+}
